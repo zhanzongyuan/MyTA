739707bcce95d0885bb3ec549f8d5628d1481cdb
HinanawiTenshi <dr.paper@live.com>
2017-12-18 17:17:08 +0800

    Update many 1. Update gradle: add rxjava2 support 2. Complete LoginActivity and RegisterActivity

--- app/src/main/java/four/awesome/myta/models/User.java
+++ app/src/main/java/four/awesome/myta/models/User.java
@@ -5,23 +5,21 @@ package four.awesome.myta.models;
  */
 
 public class User {
-    public static int TYPE_STUDENT = 0, TYPE_TEACHER = 1;
-
     private String username;
     private String password;
-    private int type;
+    private String type;
     private String email;
     private String token;
 
     public String getUsername() { return username; }
     public String getPassword() { return password; }
-    public int getType() { return type; }
+    public String getType() { return type; }
     public String getEmail() { return email; }
     public String getToken() { return token; }
 
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
-    public void setType(int type) { this.type = type; }
+    public void setType(String type) { this.type = type; }
     public void setEmail(String email) { this.email = email; }
     public void setToken(String token) { this.token = token; }
 }
--- app/src/main/java/four/awesome/myta/services/APIAccessService.java
+++ app/src/main/java/four/awesome/myta/services/APIAccessService.java
@@ -1,14 +1,21 @@
 package four.awesome.myta.services;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 
 import four.awesome.myta.models.User;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 /**
@@ -22,9 +29,10 @@ public class APIAccessService {
     private RetrofitService service;
 
     public interface RetrofitService {
+        @FormUrlEncoded
         @POST("auth")
-        Call<User> authorize(@Field("username") String username,
-                             @Field("password") String password);
+        Observable<User> authorize(@Field("username") String username,
+                                   @Field("password") String password);
     }
 
     public APIAccessService() {
@@ -38,15 +46,19 @@ public class APIAccessService {
     }
 
     private void createRetrofitService() {
-        String HOST = "https://private-ee70cb-myta.apiary-mock.com/v1";
+        String HOST = "https://private-ee70cb-myta.apiary-mock.com/v1/";
         service = new Retrofit.Builder()
                 .baseUrl(HOST)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
                 .create(RetrofitService.class);
     }
 
-    public Call<User> authorize(String username, String password) {
-        return service.authorize(username, password);
+    public void subcribeAuthorize(Observer<User> observer, String username, String password) {
+        service.authorize(username, password)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
     }
 }
